ea22588af0954d02cff958a9a58f230cd40c7cfe
Oscar Raygoza <oscar.raygoza@gmail.com>
2023-11-20 13:25:06 -0600

    Barra de navegacion principal

--- modulos/nav-bar-main.php
+++ modulos/nav-bar-main.php
@@ -1,12 +1,18 @@
 
 <div class="nav-background">
     <nav class="container">
-        <ul>
-            <li><a href="#">Médico</a></li>
-            <li><a href="#">Médico</a></li>
-            <li><a href="#">Médico</a></li>
-            <li><a href="#">Médico</a></li>
-            <li><a href="#">Médico</a></li>
+        <ul class="menu_principal">
+            <li><a href="/index.php">Inicio</a></li>
+            <li><a href="#">Administración</a></li>
+            <li><a href="#">Compras</a></li>
+            <li><a href="#">Recetas</a></li>
+            <li><a href="#">Ventas</a></li>
+            <li><a href="#">Citas Médicas</a></li>
+            <li><a href="#">Stock</a></li>
+            <li><a href="#">Informes</a></li>
+            <li><a href="#">Archivos</a></li>
+            <li><a href="#">Médicos</a></li>
+            <li><a href="#">Enlace Médico</a></li>
         </ul>
     </nav>
 </div>
