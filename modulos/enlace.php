3422f907ee2d38a8b4754f6f220896ed71b1d0e8
Oscar Raygoza <oscar.raygoza@gmail.com>
2023-11-21 20:53:19 -0600

    Agregando Modulos

---
+++ modulos/enlace.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/estilos.css">
+    <title>Enlace Médico</title>
+</head>
+<header>
+    <?php require '../modulos/nav-bar-main.php' ?>
+</header>
+<body>
+    <div class="container">
+        <img class="img_under" src="/img/construccion.svg">
+    </div>
+
+</body>
+    <?php require '../modulos/footer_main.php' ?>
+</html>
--- modulos/nav-bar-main.php
+++ modulos/nav-bar-main.php
@@ -3,16 +3,16 @@
     <nav class="container">
         <ul class="menu_principal">
             <li><a href="/mercurio/index.php">Inicio</a></li>
-            <li><a href="#">Administración</a></li>
-            <li><a href="#">Compras</a></li>
-            <li><a href="#">Recetas</a></li>
-            <li><a href="#">Ventas</a></li>
-            <li><a href="#">Citas Médicas</a></li>
-            <li><a href="#">Stock</a></li>
-            <li><a href="#">Informes</a></li>
-            <li><a href="#">Archivos</a></li>
-            <li><a href="#">Médicos</a></li>
-            <li><a href="#">Enlace Médico</a></li>
+            <li><a href="../modulos/administracion.php">Administración</a></li>
+            <li><a href="../modulos/compras.php">Compras</a></li>
+            <li><a href="../modulos/recetas.php">Recetas</a></li>
+            <li><a href="../modulos/ventas.php">Ventas</a></li>
+            <li><a href="../modulos/citas.php">Citas Médicas</a></li>
+            <li><a href="../modulos/stock.php">Stock</a></li>
+            <li><a href="../modulos/informes.php">Informes</a></li>
+            <li><a href="../modulos/archivos.php">Archivos</a></li>
+            <li><a href="../modulos/medicos.php">Médicos</a></li>
+            <li><a href="../modulos/enlace.php">Enlace Médico</a></li>
         </ul>
     </nav>
 </div>
